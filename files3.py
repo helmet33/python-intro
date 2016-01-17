402310a98ce8f5dad0db0a77351570438204c0f6
Garrett Jordan <garrettjor@gmail.com>
2016-01-17 03:11:01 +0000

    Comments and cleanup

--- files3.py
+++ files3.py
@@ -1,4 +1,8 @@
+# Garrett Jordan
+
 # Function to check if the word is an anagram
+# Creates lists from both strings and sorts them
+# Returns the result of comparison
 def isAnAnagram(word, user):
     wordAsList= list(word)
     wordAsList.sort()
@@ -7,21 +11,28 @@ def isAnAnagram(word, user):
 
     return (wordAsList == inputAsList)
 
+# reads from files to list and closes file
 with open('wordlist.txt', 'r') as f:
 	allwords = f.readlines()
 f.close()
 
+# Creates a list of words from the files
+# Strips new line
 words = []
 for x in allwords:
 	x = x.rstrip()
 	words.append(x)
 
-
+# Takes input
 inp = input("enter word:")
+# Should really check type here...
+# Creates a list from words with same length as user input
 lister = [word for word in words if len(word) == len(inp) ]
+# Loops through words of same length and uses function
+# to check if they are anagrams.  Excludes word itself
 for item in lister:
 	if item != inp:
 		if isAnAnagram(item, inp):
 			print(item)
 
-#print(lister)
+#Fin
